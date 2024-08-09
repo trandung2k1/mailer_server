ba290d790800685db42f8870f3bf71fa3411ec8c
trandung2k1 <trandungitksnb2001@gmail.com>
2024-08-09 23:46:04 +0700

    update client

--- express_mailer/server.js
+++ express_mailer/server.js
@@ -3,38 +3,38 @@ const express = require('express');
 const nodemailer = require('nodemailer');
 const { USER, PASSWORD, HOST, PORT_MAIL } = process.env;
 const transport = nodemailer.createTransport({
-  host: HOST,
-  port: PORT_MAIL,
-  secure: false,
-  auth: {
-    user: USER,
-    pass: PASSWORD,
-  },
+    host: HOST,
+    port: +PORT_MAIL,
+    secure: false,
+    auth: {
+        user: USER,
+        pass: PASSWORD,
+    },
 });
-// console.log(process.env.NODE_ENV);
+console.log(USER, PASSWORD);
 const app = express();
 const port = process.env.PORT || 4000;
 transport.verify((error, success) => {
-  if (error) {
-    console.log(error);
-  } else {
-    console.log('Ready for message');
-    console.log(success);
-  }
+    if (error) {
+        console.log(error);
+    } else {
+        console.log('Ready for message');
+        console.log(success);
+    }
 });
 app.get('/', async (req, res) => {
-  try {
-    const rs = await transport.sendMail({
-      from: 'trandungksnb00@gmail.com',
-      subject: 'Hello',
-      to: 'trandungitksnb2001@gmail.com',
-      html: '<h2>Hello</h2>',
-    });
-    return res.status(200).json(rs);
-  } catch (error) {
-    return res.status(500).json(error);
-  }
+    try {
+        const rs = await transport.sendMail({
+            from: 'trandungksnb00@gmail.com',
+            subject: 'Hello',
+            to: 'trandungitksnb2001@gmail.com',
+            html: '<h2>Hello</h2>',
+        });
+        return res.status(200).json(rs);
+    } catch (error) {
+        return res.status(500).json(error);
+    }
 });
 app.listen(port, () => {
-  console.log(`Server listening on http://localhost:${port}`);
+    console.log(`Server listening on http://localhost:${port}`);
 });
